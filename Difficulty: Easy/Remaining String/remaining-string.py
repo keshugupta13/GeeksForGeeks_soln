836366589a3bba092fff5822999fccdee17d410a
KESHU GUPTA13 <113796557+keshugupta13@users.noreply.github.com>
2024-07-16 19:36:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remaining String/remaining-string.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+class Solution:
+	def printString(self, s, ch, count):
+	    for i in range(len(s)):
+            if s[i] == ch:
+                count -= 1
+                if count == 0:
+                    return s[i+1:]
+        return ""
+	    
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    T = int(input())
+    for i in range(T):
+        s = input()
+        ch = input()[0]
+        count = int(input())
+        ob = Solution()
+        answer = ob.printString(s, ch, count)
+
+        print(answer)
+
+# } Driver Code Ends
