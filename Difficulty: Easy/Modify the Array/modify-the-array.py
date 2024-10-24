0f7c8648f53eb3a92a9af6b3c4449a810cd407ce
KESHU GUPTA13 <113796557+keshugupta13@users.noreply.github.com>
2024-10-24 12:44:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Modify the Array/modify-the-array.py
@@ -0,0 +1,46 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def modifyAndRearrangeArr (self, arr) :
+        n = len(arr)
+        for i in range(n-1):
+            if arr[i] != 0 and arr[i] == arr[i+1]:
+                arr[i] *= 2
+                arr[i+1]  = 0
+                
+        arr1 = []
+        for i in range(n):
+            if arr[i] != 0:
+                arr1.append(arr[i])
+                
+        arr1 += [0] * (n - len(arr1))
+        return arr1
+            
+        
+                
+        
+                
+        #Complete the function
+
+
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        ans = ob.modifyAndRearrangeArr(arr)
+        print(*ans)
+        print("~")
+        t -= 1
+
+
+# } Driver Code Ends
